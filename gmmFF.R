2fbba75c60018ed0e5aaf0de5bd88152d5a71918
eillasti <d.g.kornilov@gmail.com>
2015-02-22 04:57:39 +0100

    cross-sectional regressions aggregated by portfolio

--- gmmFF.R
+++ gmmFF.R
@@ -16,9 +16,28 @@ dtFF[, MKT := mean(Returns, na.rm = TRUE), by = "Date"]
 
 #Estimate betas
 dtFF[, betaMKT := coef(lm(Returns ~ MKT + HML + SMB, .SD))[2], by = c("PortfolioPrice", "PortfolioSize")]
+# dtFF[, betaMKT := coef(lm(Returns ~ MKT))[2], by = c("PortfolioPrice", "PortfolioSize")]
 dtFF[, betaHML := coef(lm(Returns ~ MKT + HML + SMB, .SD))[3], by = c("PortfolioPrice", "PortfolioSize")]
-dtFF[, betaFMB := coef(lm(Returns ~ MKT + HML + SMB, .SD))[4], by = c("PortfolioPrice", "PortfolioSize")]
+dtFF[, betaSMB := coef(lm(Returns ~ MKT + HML + SMB, .SD))[4], by = c("PortfolioPrice", "PortfolioSize")]
 
-m = lm(Returns ~ betaMKT + betaHML + betaFMB, dtFF)
-
+#Pooled cross-section
+m = lm(Returns ~ betaMKT + betaHML + betaSMB, dtFF)
 summary(m)
+
+#By portfolio
+gmean = function(x, na.rm = FALSE) {
+  exp(mean(log(x + 1), na.rm = na.rm)) - 1
+}
+dtFF_CS = dtFF[, list(Returns = gmean(Returns, na.rm = TRUE),
+                      betaMKT = betaMKT[1],
+                      betaHML = betaHML[1],
+                      betaSMB = betaSMB[1]),
+               keyby = c("PortfolioPrice", "PortfolioSize")]
+modelCS = lm(Returns * 252 ~ betaMKT + betaHML + betaSMB, dtFF_CS)
+summary(modelCS)
+
+bySMB = dtFF_CS[, list(betaMKT = mean(betaMKT), Returns = mean(Returns)), by = "PortfolioSize"]
+byHML = dtFF_CS[, list(betaMKT = mean(betaMKT), Returns = mean(Returns)), by = "PortfolioPrice"]
+plot(bySMB$betaMKT, bySMB$Returns * 252)
+plot(byHML$betaMKT, byHML$Returns * 252)
+plot(dtFF_CS$betaMKT, dtFF_CS$Returns * 252)
