4c01e01fcc11ed1a9172132e0d89774a8171d9df
eillasti <d.g.kornilov@gmail.com>
2015-03-09 19:12:43 +0100

    data casting and preliminary calculations

--- dataPrep.R
+++ dataPrep.R
@@ -1,119 +1,81 @@
-library(data.table)
-library(dplyr)
-library(zoo)
+load("../Data/spot.RData")
+load("../Data/fwd.RData")
+SR = function(x, n = 252) mean(x, na.rm = TRUE) / sd(x, na.rm = TRUE) * sqrt(n)
+portfolioMean = function(rgeom){
+  Rarithm = exp(rgeom)
+  log(mean(Rarithm))
+}
+fwd1m = fwd[maturity == "1M", list(fwd = fwd), c("asset", "date")]
 
 
-spot = fread("../data/newData/spot.csv")
-NZD = fread("../data/newData/NZD.csv")
-spot = rbind(spot, NZD[ticker == "NZD Curncy"])
-spot = spot[ticker != "GHS Curncy"]
-
-spot[, dupl := seq_along(px_last) > 1, by = c("ticker", "date")]
-spot = spot[dupl != TRUE]
-spot[, dupl := NULL]
-
-spot[, date := as.Date(date, "%Y-%m-%d")]
-spot[is.na(px_last) & (!is.na(px_bid) | !is.na(px_ask)), px_last := ifelse(is.na(px_bid), px_ask, ifelse(is.na(px_ask), px_bid, (px_ask + px_bid)/2))]
-spot[, asset := substr(ticker, 1, 3)]
-spot[asset %in% c("EUR", "GBP", "AUD", "NZD"), px_last := 1/px_last]
-spot[, spot := px_last]
-setkey(spot, asset, date)
-
-fwd = fread("../data/newData/fwd.csv")
-fwd = rbind(fwd, NZD[ticker != "NZD Curncy"])
-fwd[, date := as.Date(date, "%Y-%m-%d")]
-fwd[, dupl := seq_along(px_last) > 1, by = c("ticker", "date")]
-fwd = fwd[dupl != TRUE]
-fwd[, dupl := NULL]
-fwd[is.na(px_last) & (!is.na(px_bid) | !is.na(px_ask)), px_last := ifelse(is.na(px_bid), px_ask, ifelse(is.na(px_ask), px_bid, (px_ask + px_bid)/2))]
-fwd[, asset := substr(ticker, 1, 3)]
-fwd[, maturity := substr(ticker, 4, 5)]
-fwd[asset %in% c("EUR", "GBP", "AUD", "NZD"), px_last := 1/px_last]
-
-fwd1m = fwd[maturity == "1M", list(fwd = px_last), c("asset", "date")]
-
+###Monthly returns with accurate forward rates (more or less)
 monthlyReturns = merge(spot[, list(asset, date, spot)], fwd1m, by = c("asset", "date"))
 monthlyReturns[, date := as.yearmon(date)]
 # monthlyReturns = monthlyReturns[!is.na(fwd)]
 monthlyReturns = monthlyReturns[, list(spot = spot[1], fwd = fwd[1]), keyby = c("asset", "date")]
-monthlyReturns[, rx := log(spot) - stats::lag(log(fwd)), by = asset]
+monthlyReturns[, rx := (log(spot) - stats::lag(log(fwd))), by = asset]
+monthlyReturns[, rf := 12 * (log(spot) - log(fwd)), by = "asset"]
 monthlyReturns = monthlyReturns[!is.na(rx)]
-monthlyReturns
-
-monthlyReturns = monthlyReturns[, list(asset, date, rx)]
+# monthlyReturns[, mean(rx) * (12), by = "asset"][, .SD, keyby = "V1"]
+# monthlyReturns[asset == "AUD", mean(rf)]
+monthlyReturns = monthlyReturns[, list(asset, date, rx, rf)]
 save(monthlyReturns, file = "../Data/monthlyReturns.RData")
 
-###Daily spot returns
+###Daily returns with inaccurate interest rates
+dailyReturns = spot[!is.na(px_last), spot, keyby = c("asset", "date")]
+dailyReturns[, rx := log(spot) - stats::lag(log(spot)), by = "asset"]
+dailyReturns = merge(dailyReturns,  fwd1m)
+dailyReturns[, rf := (log(spot) - log(fwd)) * 12]
+dailyReturns[, rf := as.numeric(stats::filter(rf, rep(1/22,22), sides = 1)), by = "asset"]
+dailyReturns[, rx := rx + as.numeric(date - stats::lag(date)) * rf / 365, by = "asset"]
+dailyReturns = dailyReturns[!is.na(rx)]
+dailyReturns = dailyReturns[, list(asset, date,  rx, rf)]
+dailyReturns
+save(dailyReturns, file = "../Data/dailyReturns.RData")
 
-spotReturns = spot[!is.na(px_last), spot, keyby = c("asset", "date")]
-spotReturns[, rx := log(spot) - stats::lag(log(spot)), by = asset]
-spotReturns = spotReturns[!is.na(rx)]
-spotReturns = merge(spotReturns,  fwd1m)
-spotReturns[, rf := (log(fwd) - log(spot)) * 12]
-spotReturns = spotReturns[, list(asset, date,  rx, rf)]
-spotReturns
-save(spotReturns, file = "../Data/spotReturns.RData")
-
-
-# spotReturns[, t1 := as.numeric(date - min(date))]
-# spotReturns[, rfn := (rf - mean(rf, na.rm = TRUE)) / ifelse(.N != 1, sd(rf, na.rm = TRUE), 1), by = "date"]
-
-# spotReturns[asset == "JPY" & rf > 0]
 
 ###Risk factors
 
-#volatilities
-vols = fread("../data/newData/vols.csv")
-vols[, date := as.Date(date, "%Y-%m-%d")]
-vols[, asset := substr(ticker, 1,3)]
-vols[asset == "USD", asset := substr(ticker, 4, 6)]
-vols[grepl("V",substr(ticker, 7, 12)), strike := "ITM"]
-vols[grepl("25B",substr(ticker, 7, 12)), strike := "25B"]
-vols[grepl("25R",substr(ticker, 7, 12)), strike := "25R"]
-vols[grepl("10B",substr(ticker, 7, 12)), strike := "10B"]
-vols[grepl("10R",substr(ticker, 7, 12)), strike := "10R"]
-vols[is.na(strike)]
+###DOL
+dtDOL = dailyReturns[, list(DOL = portfolioMean(rx)), by = "date"]
 
-vols[grepl("ON",substr(ticker, 7, 12)), maturity := "ON"]
-vols[grepl("1W",substr(ticker, 7, 12)), maturity := "1W"]
-vols[grepl("1M",substr(ticker, 7, 12)), maturity := "1M"]
-vols[grepl("3M",substr(ticker, 7, 12)), maturity := "3M"]
-vols[grepl("6M",substr(ticker, 7, 12)), maturity := "6M"]
-vols[grepl("9M",substr(ticker, 7, 12)), maturity := "9M"]
-vols[grepl("1Y",substr(ticker, 7, 12)), maturity := "1Y"]
-vols[grepl("2Y",substr(ticker, 7, 12)), maturity := "2Y"]
-vols[grepl("5Y",substr(ticker, 7, 12)), maturity := "5Y"]
-vols[is.na(maturity)]
+###Carry-trade portfolio risk factor
+dtCT = copy(dailyReturns)
+dtCT[, portfolio := paste0("CT", ntile(rf, 5)), by = "date"]
+# dtCT[, SR(rx), by = portfolio]
+# table(dtCT$asset, dtCT$portfolio)
+dtCT = data.table(cast(dtCT, date ~ portfolio, value = "rx", fun.aggregate = portfolioMean))
+dtCT = dtCT[!is.na(CT5)]
+dtCT[, CT := log(exp(CT5) - exp(CT1) + 1)]
+# SR(dtCT1$CT)
+# SR(dtCT1$CT5 - dtCT1$CT1)
+dtCT = dtCT[, list(date, CT)]
 
-vols[is.na(px_last) & (!is.na(px_bid) | !is.na(px_ask)), px_last := ifelse(is.na(px_bid), px_ask, ifelse(is.na(px_ask), px_bid, (px_ask + px_bid)/2))]
+# plot(dtCT1$date, exp(cumsum(dtCT1$CT)), type = "l")
 
-volFX = vols[strike == "ITM" & maturity == "1M", list(vol = mean(px_last, na.rm = TRUE) / 100), keyby = date]
-volFX
-
-vixFX = fread("../data/newData/JPM_FX_VIX.csv")  
-vixFX[, date := as.Date(date, "%Y-%m-%d")]
-
-vixSP = fread("../data/newData/VIX.csv")
-vixSP[, date := as.Date(date, "%Y-%m-%d")]
-
-vix = merge(vixSP[, list(date, vixSP = px_last/100)], 
-            vixFX[, list(date, vixFX = px_last/100)],
-            by = "date", all = TRUE)
-vix = merge(vix,
-            volFX[, list(date, ivFX = vol)],
-            by = "date", all = TRUE)
-
+###Volatility indices
+load("../Data/dtVix.RData")
 source("HAR.R")
-vix[!is.na(vixSP), rVixSP := HARres(vixSP)]
-vix[!is.na(vixFX), rVixFX := HARres(vixFX)]
-vix[!is.na(ivFX), rIvFX := HARres(ivFX)]
+dtVix[!is.na(vixSP), rVixSP := HARres(vixSP)]
+dtVix[!is.na(vixFX), rVixFX := HARres(vixFX)]
+dtVix[!is.na(ivFX), rIvFX := HARres(ivFX)]
 
-m1 = lm(vixFX ~ vixSP, vix)
+m1 = lm(vixFX ~ vixSP, dtVix)
 summary(m1)
 
-m1 = lm(vixFX ~ ivFX, vix)
+m1 = lm(vixFX ~ ivFX, dtVix)
 summary(m1)
 
-m1 = lm(rVixFX ~ rVixSP, vix)
+m1 = lm(rVixFX ~ rVixSP, dtVix)
 summary(m1)
-vix[, rVixFXres := (rVixFX - predict(m1, .SD))]
+dtVix[, rVixFXres := (rVixFX - predict(m1, .SD))]
+
+### FF factors
+load("../Data/dtFF.RData")
+
+###Risk factors
+dtFactors = merge(dtDOL, dtCT, by = "date", all = TRUE)
+dtFactors = merge(dtFactors, dtVix, by = "date", all = TRUE)
+dtFactors = merge(dtFactors, dtFF, by = "date", all = TRUE)
+
+save(dtFactors, file = "../Data/dtFactors.RData")
